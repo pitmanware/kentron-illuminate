a98ede95660eb09bbed6b1ea92b347da897d9aea
Ben Pitman <pitman.ben@gmail.com>
2020-11-26 00:42:31 +0000

    initial commit

---
+++ src/Template/AModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kentron\Illuminate\Template;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AModel extends Model
+{
+    public $timestamps = false;
+
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
+    const DELETED_AT = null;
+}
---
+++ src/Template/TSoftDeletes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kentron\Illuminate\Template;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+trait TSoftDeletes
+{
+    use SoftDeletes;
+}
