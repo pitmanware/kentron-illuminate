c0a194aa7359fb298281ab44731804a90366d9cf
Ben Pitman <pitman.ben@gmail.com>
2020-11-30 00:23:55 +0000

    Working on Phinx wrapper

---
+++ src/Facade/Phinx/Skeleton.php
@@ -0,0 +1,28 @@
+<?php
+
+$namespaceDefinition
+
+use $useClassName;
+
+final class $className extends $baseClassName
+{
+    /**
+     * Migrate method
+     *
+     * @return void
+     */
+    public function up (): void
+    {
+
+    }
+
+    /**
+     * Rollback method
+     *
+     * @return void
+     */
+    public function down (): void
+    {
+
+    }
+}
---
+++ src/Facade/Phinx/Template/AMigration.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Kentron\Facade\Phinx\Template;
+
+use Phinx\Migration\AbstractMigration;
+
+abstract class AMigration extends AbstractMigration
+{
+    /**
+     * Returns the length of the column provided
+     * @param  string   $table  The table in which the column belongs
+     * @param  string   $column The column in question
+     * @return null|int         Null if the table/column does not exist
+     */
+    protected function getColumnLength (string $table, string $column): ?int
+    {
+        if (!$this->hasTable($table)) {
+            return null;
+        }
+
+        if (!$this->table($table)->hasColumn($column)) {
+            return null;
+        }
+
+        $response = $this->fetchRow(
+            "SELECT
+                CHARACTER_MAXIMUM_LENGTH
+            FROM
+                `information_schema`.columns
+            WHERE
+                table_name = '$table'
+            AND
+                COLUMN_NAME = '$column'"
+        );
+
+        return $response["CHARACTER_MAXIMUM_LENGTH"] ?? null;
+    }
+}
